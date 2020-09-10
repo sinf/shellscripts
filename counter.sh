7219ff04affe0066cc4f54204196922b70d0fd47
ArhoM <arho.iivari@elisanet.fi>
2020-09-10 13:48:04 +0300

    counter w/ 4 numbers

--- counter.sh
+++ counter.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-echo Press q to stop, any other key to increment the counter
-x=0
+echo Press
+echo ... a/b/c/d to increment a counter
+echo ... A/B/C/D to decrement a counter
+echo ... q to stop
+echo
+a=0
+b=0
+c=0
+d=0
 while true; do
-	printf '\r%d' $x
+	printf '\r%4d  + %4d  + %4d  + %4d  = %6d    ' $a $b $c $d $((a + b + c + d))
 	read -n 1 -s -r k
-	if [ "$k" = "q" ]; then break; fi
-	x=$((x + 1))
+	case "$k" in
+		a)
+			a=$((a + 1)) ;;
+		A)
+			a=$((a - 1)) ;;
+		b)
+			b=$((b + 1)) ;;
+		B)
+			b=$((b - 1)) ;;
+		c)
+			c=$((c + 1)) ;;
+		C)
+			c=$((c - 1)) ;;
+		d)
+			d=$((d + 1)) ;;
+		D)
+			d=$((d - 1)) ;;
+		q)
+			break ;;
+	esac
 done
 echo
 
